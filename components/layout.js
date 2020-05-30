e623d28ada3cac7628f83a0393c62994d364508c
kazuto0291 <zutozuto03170291@gmail.com>
2020-05-30 15:10:23 +0900

    cssモジュールを使用

--- components/layout.js
+++ components/layout.js
@@ -1,5 +1,11 @@
-function Layout({ children }) {
-  return <div>{children}</div>
+import styles from './layout.module.css'
+
+export default function Layout({ children }) {
+  return <div className={styles.container}>{children}</div>
 }
 
-export default Layout
+// function Layout({ children }) {
+//   return <div>{children}</div>
+// }
+
+// export default Layout
