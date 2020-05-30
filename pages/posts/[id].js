7e47899a7cb0be0ed60b03fad7b7da0ff831ffb9
kazuto0291 <zutozuto03170291@gmail.com>
2020-05-30 18:20:29 +0900

    date-fns ライブラリinstall
    日付をフォーマットするため

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,10 +1,14 @@
 import Layout from '../../components/layout'
-// import { getAllPostIds } from '../../lib/posts'
 import { getAllPostIds, getPostData } from '../../lib/posts'
+import Head from 'next/head'
+
 
 export default function Post({ postData }) {
   return (
     <Layout>
+       <Head>
+        <title>{postData.title}</title>
+      </Head>
       {postData.title}
       <br />
       {postData.id}
